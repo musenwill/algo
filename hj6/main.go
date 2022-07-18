2386a111307adc785483522febd0d29ac0460b60
musenwill <musenwill@qq.com>
2022-07-18 19:19:04 +0800

    .

--- hj6/main.go
+++ hj6/main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 func main() {
 	var num int
 	_, _ = fmt.Scan(&num)
+	sqrt := int(math.Sqrt(float64(num)))
 
 	maxFactor := 2
 
@@ -19,10 +21,14 @@ func main() {
 				fmt.Printf("%d ", i)
 				break
 			}
-			if i&0x01 == 0x01 {
-				i += 2
+			if i <= sqrt {
+				if i&0x01 == 0x01 {
+					i += 2
+				} else {
+					i++
+				}
 			} else {
-				i++
+				i = num
 			}
 		}
 	}
