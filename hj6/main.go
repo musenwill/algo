bfdc430a8753322f9cffde7f9b5122c84c84fe54
musenwill <musenwill@qq.com>
2022-07-19 10:12:22 +0800

    .

--- hj6/main.go
+++ hj6/main.go
@@ -10,26 +10,19 @@ func main() {
 	_, _ = fmt.Scan(&num)
 	sqrt := int(math.Sqrt(float64(num)))
 
-	maxFactor := 2
-
-	for num > 1 {
-		i := maxFactor
-		for i <= num {
-			if num%i == 0 {
-				num /= i
-				maxFactor = i
-				fmt.Printf("%d ", i)
-				break
-			}
-			if i <= sqrt {
-				if i&0x01 == 0x01 {
-					i += 2
-				} else {
-					i++
-				}
-			} else {
-				i = num
-			}
+	for i := 2; i <= sqrt; {
+		for num%i == 0 {
+			num /= i
+			fmt.Printf("%d ", i)
 		}
+		if i&0x01 == 0x01 {
+			i += 2
+		} else {
+			i++
+		}
+	}
+
+	if num > 1 {
+		fmt.Print(num)
 	}
 }
